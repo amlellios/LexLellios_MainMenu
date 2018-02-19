8fee817ebc63a7e68076cf1a3da52c59c18a18e2
amlellios <alexis.lellios@loop.colum.edu>
2018-02-18 22:53:31 -0600

    main menu tutorial

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EndGame : MonoBehaviour
+{
+    public void Quit();
+        }
+
+
+
+
+#if UNITY_EDITOR
+UnityEditor.EditiorApplication.isPlaying=false;
+
+#else
+    Application.Quit();
+
+#endif
+        
+
